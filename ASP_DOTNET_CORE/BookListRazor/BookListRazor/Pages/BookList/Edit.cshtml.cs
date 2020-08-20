a767793f8a6e60ff1840ab2cd0127514480414a1
jhpp114 <jhpp114@uw.edu>
2020-08-19 23:33:39 -0700

    learning CRUD on Dot Net Core

---
+++ ASP_DOTNET_CORE/BookListRazor/BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            try
+            {
+                var editTarget = await _db.Book.FindAsync(Book.Id);
+                editTarget.Book_Name = Book.Book_Name;
+                editTarget.Author = Book.Author;
+                editTarget.ISBN = Book.ISBN;
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Page();
+            }
+        }
+    }
+}
---
+++ ASP_DOTNET_CORE/BookListRazor/BookListRazor/Pages/BookList/index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class indexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public indexModel(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+    }
+}
