040a07076c35b534d62af2af5340d3db78e32654
jhpp114 <jhpp114@uw.edu>
2020-08-22 21:59:14 -0700

    starting on spice category

---
+++ Spice/Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCategory = await _db.Category.ToListAsync();
+            return View(allCategory);
+        }
+    }
+}
