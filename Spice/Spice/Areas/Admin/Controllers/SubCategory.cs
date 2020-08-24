e2935987f6f2e2c871a3eb88d7d40f94e7e8ef87
jhpp114 <jhpp114@uw.edu>
2020-08-23 20:43:06 -0700

    prepare for subcategory

---
+++ Spice/Spice/Areas/Admin/Controllers/SubCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategory : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public SubCategory(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allSubCategoryData = await _db.SubCategory.Include(s => s.Category).ToListAsync();
+            return View(allSubCategoryData);
+        }
+    }
+}
