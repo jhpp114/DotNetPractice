2877c8a4fb68e2224a6b845f4ce85f86ba3c411f
jhpp114 <jhpp114@uw.edu>
2020-08-26 21:15:28 -0700

    blazor component

---
+++ BlazorMovies/BlazorMovies/Client/Helpers/StringUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public class StringUtilities
+    {
+        public static string CustomUpper(string val)
+        {
+            return val.ToUpper();
+        }
+    }
+}
