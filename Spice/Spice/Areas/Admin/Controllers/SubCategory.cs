26e398737c57409502342da8adc3a3ec1a8ea9f1
jhpp114 <jhpp114@uw.edu>
2020-08-28 15:04:37 -0700

    subcategory edit

--- Spice/Spice/Areas/Admin/Controllers/SubCategory.cs
+++ Spice/Spice/Areas/Admin/Controllers/SubCategory.cs
@@ -77,5 +77,66 @@ namespace Spice.Areas.Admin.Controllers
             return Json(new SelectList(subcategoryItems, "Id", "Name"));
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var editSubcategory = await _db.SubCategory.FindAsync(id);
+            if (editSubcategory == null)
+            {
+                return NotFound();
+            }
+            CategoryAndSubCategoryViewModel subcategoryObj = new CategoryAndSubCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync()
+              ,
+                SubCategory = editSubcategory
+              ,
+                SubCategoryList = await _db.SubCategory.OrderBy(s => s.Name).Select(s => s.Name).ToListAsync()
+            };
+            return View(subcategoryObj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int? id, CategoryAndSubCategoryViewModel _editData)
+        {
+            if (ModelState.IsValid)
+            {
+                var isDuplicate = await _db.SubCategory.Include(s => s.Category)
+                                                        .Where(s => s.Category.Id == _editData.SubCategory.CategoryId && s.Name == _editData.SubCategory.Name)
+                                                        .ToListAsync();
+                if (isDuplicate.Count() > 0)
+                {
+                    StatusMessage = StatusMessage = "Error: This Subcategory is already exist in " + isDuplicate.First().Category.Name;
+                }
+                else
+                {
+                    try
+                    {
+                        var foundData = await _db.SubCategory.FindAsync(id);
+                        foundData.Name = _editData.SubCategory.Name;
+                        await _db.SaveChangesAsync();
+                        return RedirectToAction("Index");
+                    } catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        return RedirectToAction("Index");
+                    }
+                   
+                }
+            }
+            var errorSubCategory = new CategoryAndSubCategoryViewModel()
+            {
+                CategoryList =  await _db.Category.ToListAsync()
+            ,   SubCategory = _editData.SubCategory
+            ,   SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync()
+            ,   StatusMessage = StatusMessage
+            };
+            return View(errorSubCategory);
+
+        }
     }
 }
