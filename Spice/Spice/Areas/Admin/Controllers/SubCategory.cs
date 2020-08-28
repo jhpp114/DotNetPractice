0a82e42536b895c484a60f549ee5d48959bb4bd8
jhpp114 <jhpp114@uw.edu>
2020-08-27 22:01:30 -0700

    create post subcategory

--- Spice/Spice/Areas/Admin/Controllers/SubCategory.cs
+++ Spice/Spice/Areas/Admin/Controllers/SubCategory.cs
@@ -37,5 +37,32 @@ namespace Spice.Areas.Admin.Controllers
                                                             .ToListAsync();
             return View(categoryAndSubViewModel);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryAndSubCategoryViewModel _subcategoryData)
+        {
+            var doesExistAlready = _db.SubCategory.Include(s => s.Category)
+                                                  .Where(s => s.Name == _subcategoryData.SubCategory.Name && s.Category.Id == _subcategoryData.SubCategory.CategoryId);
+            if (ModelState.IsValid)
+            {
+                if (doesExistAlready.Count() > 0)
+                {
+                    // error
+                }
+                else
+                {
+                    await _db.SubCategory.AddAsync(_subcategoryData.SubCategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            var errorSubCategory = new CategoryAndSubCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync()
+            ,   SubCategory = _subcategoryData.SubCategory
+            ,   SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync()
+            };
+            return View(errorSubCategory);
+        }
     }
 }
--- Spice/Spice/Models/Category.cs
+++ Spice/Spice/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Spice.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category")]
         public string Name { get; set; }
     }
 }
