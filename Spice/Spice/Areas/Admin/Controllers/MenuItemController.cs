8ff5ca95b7710d227fd2997c52f598a6745059a1
jhpp114 <jhpp114@uw.edu>
2020-08-28 23:01:06 -0700

    created model for menuitem and start controller

---
+++ Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webhost;
+        
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment webhost)
+        {
+            _db = db;
+            _webhost = webhost;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allMenuItems = await _db.MenuItem.Include(s => s.Category).Include(s => s.SubCategory).ToListAsync();
+            return View(allMenuItems);
+        }
+    }
+}
---
+++ Spice/Spice/Models/MenuItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class MenuItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public string Spicyness { get; set; }
+        public enum Spicy_Enum { NotSpicy, Level1, Level2, Level3 }
+        public string Image { get; set; }
+        // Foreign Keys
+        [DisplayName("Category")]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        [DisplayName("Subcategory")]
+        public int SubCategoryId { get; set; } 
+        [ForeignKey("SubCategoryId")]
+        public virtual SubCategory SubCategory { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage ="It has to be bigger than $1")]
+        public double Price { get; set; }
+    }
+}
