52c196ce841404b079e65f3c278fed56764be53d
jhpp114 <jhpp114@uw.edu>
2020-08-31 00:11:23 -0700

    working on detail views for menuitem

--- Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
+++ Spice/Spice/Areas/Admin/Controllers/MenuItemController.cs
@@ -74,7 +74,7 @@ namespace Spice.Areas.Admin.Controllers
                 {
                     files[0].CopyTo(fileStream);
                 }
-                menuItemFromDb.Image = @"\images\" + _MenuitemViewModel.MenuItem.Id + extension;
+                menuItemFromDb.Image = @"\Images\" + _MenuitemViewModel.MenuItem.Id + extension;
             }
             else
             {
@@ -95,6 +95,65 @@ namespace Spice.Areas.Admin.Controllers
             }
             _MenuitemViewModel.MenuItem = await _db.MenuItem.Include(s => s.Category).Include(s => s.SubCategory).SingleOrDefaultAsync(s => s.Id == id);
             if (_MenuitemViewModel.MenuItem == null)
+            {
+                return NotFound();
+            }
+            return View(_MenuitemViewModel);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditPost(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(_MenuitemViewModel);
+            }
+            var webRootPath = _webhost.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            var menuitemDataFromDb = await _db.MenuItem.FindAsync(id);
+            if (files.Count() > 0)
+            {
+                // user upload new photo
+                var path = Path.Combine(webRootPath, "Images");
+                var extension = Path.GetExtension(files[0].FileName);
+                var fullPath = Path.Combine(path, _MenuitemViewModel.MenuItem.Id + extension);
+
+                // delete the original file
+                var imagePath = Path.Combine(webRootPath, menuitemDataFromDb.Image.TrimStart('\\'));
+                if (System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+                using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+                menuitemDataFromDb.Image = @"\Images\" + _MenuitemViewModel.MenuItem.Id + extension;
+            }
+            menuitemDataFromDb.Name = _MenuitemViewModel.MenuItem.Name;
+            menuitemDataFromDb.Description = _MenuitemViewModel.MenuItem.Description;
+            menuitemDataFromDb.Price = _MenuitemViewModel.MenuItem.Price;
+            menuitemDataFromDb.CategoryId = _MenuitemViewModel.MenuItem.CategoryId;
+            menuitemDataFromDb.SubCategoryId = _MenuitemViewModel.MenuItem.SubCategoryId;
+            menuitemDataFromDb.Spicyness = _MenuitemViewModel.MenuItem.Spicyness;
+            // user did not upload new photo no need to do anything
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            _MenuitemViewModel.MenuItem = await _db.MenuItem.Include(s => s.Category).Include(s => s.SubCategory).SingleOrDefaultAsync(s => s.Id == id);
+            if (_MenuitemViewModel == null)
             {
                 return NotFound();
             }
