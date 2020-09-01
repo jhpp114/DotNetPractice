d00e1acc926883159db83906883069ac6a1dde3e
jhpp114 <jhpp114@uw.edu>
2020-08-31 19:10:28 -0700

    setup model for coupon

---
+++ Spice/Spice/Areas/Admin/Controllers/CouponController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Spice/Spice/Models/Coupon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string CouponType { get; set; }
+        public enum Coupon_Enum { Percent_Coupon, Dolloar_Coupon }
+        [Required]
+        public double Discount { get; set; }
+        [Required]
+        public double MinimumAmount { get; set; }
+        [Required]
+        public byte[] CouponImage { get; set; }
+        [Required]
+        public bool isCouponActive { get; set; }
+    }
+}
