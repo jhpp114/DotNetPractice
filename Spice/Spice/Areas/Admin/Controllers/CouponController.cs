876015a8e1b60c7480414236480084b13193f19f
jhpp114 <jhpp114@uw.edu>
2020-08-31 21:32:40 -0700

    coupon index view and action method

--- Spice/Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Spice/Areas/Admin/Controllers/CouponController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Spice.Data;
 
 namespace Spice.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CouponController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public IActionResult Index()
+
+        public CouponController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allCouponData = await _db.Coupon.ToListAsync();
+            return View(allCouponData);
         }
     }
 }
