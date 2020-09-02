e10c2c8564aa1429ce1c2b44f8ffdd22d9688443
jhpp114 <jhpp114@uw.edu>
2020-09-02 00:30:50 -0700

    added customer view model and index action method

--- Spice/Spice/Areas/Customer/Controllers/HomeController.cs
+++ Spice/Spice/Areas/Customer/Controllers/HomeController.cs
@@ -4,8 +4,11 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Spice.Data;
 using Spice.Models;
+using Spice.Models.ViewModels;
 
 namespace Spice.Controllers
 {
@@ -13,15 +16,22 @@ namespace Spice.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexViewModel indexViewModel = new IndexViewModel()
+            {
+                MenuItem = await _db.MenuItem.Include(s => s.Category).Include(s => s.SubCategory).ToListAsync()
+            ,   Category = await _db.Category.ToListAsync()
+            ,   Coupon = await _db.Coupon.Where(s => s.isCouponActive == true).ToListAsync()
+            };
+            return View(indexViewModel);
         }
 
         public IActionResult Privacy()
