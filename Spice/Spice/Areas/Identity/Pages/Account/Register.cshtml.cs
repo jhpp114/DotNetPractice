c609b2b1d882df7acbcaab04ce2f267c4209260b
jhpp114 <jhpp114@uw.edu>
2020-09-05 02:42:48 -0700

    as up now all new user will be manager. Will be change soon

--- Spice/Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Spice/Spice/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Spice.Models;
+using Spice.Utility;
 
 namespace Spice.Areas.Identity.Pages.Account
 {
@@ -102,28 +103,48 @@ namespace Spice.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
-
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
-
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    // if the assigned role user is not customer then
+                    if (!await _roleManager.RoleExistsAsync(StaticDetail.CUSTOMER_END_USER))
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetail.CUSTOMER_END_USER));
                     }
-                    else
+                    if (!await _roleManager.RoleExistsAsync(StaticDetail.KITCHEN_USER))
                     {
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetail.KITCHEN_USER));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(StaticDetail.FRONTDESK_USER))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetail.FRONTDESK_USER));
+                    }
+                    if (!await _roleManager.RoleExistsAsync(StaticDetail.MANAGER_USER))
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole(StaticDetail.MANAGER_USER));
+                    }
+                    // for now just assign new user as manager
+                    await _userManager.AddToRoleAsync(user, StaticDetail.MANAGER_USER);
+
+                    //_logger.LogInformation("User created a new account with password.");
+
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    //var callbackUrl = Url.Page(
+                    //    "/Account/ConfirmEmail",
+                    //    pageHandler: null,
+                    //    values: new { area = "Identity", userId = user.Id, code = code, returnUrl = returnUrl },
+                    //    protocol: Request.Scheme);
+
+                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                    //if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    //}
+                    //else
+                    //{
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
-                    }
+                    //}
                 }
                 foreach (var error in result.Errors)
                 {
