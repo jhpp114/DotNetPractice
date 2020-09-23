00f369e15625ebf6c7dbc41b347820bea50284d1
jhpp114 <jhpp114@uw.edu>
2020-09-23 00:31:19 -0700

    order history view page and added nav link

--- Spice/Spice/Areas/Customer/Controllers/CartController.cs
+++ Spice/Spice/Areas/Customer/Controllers/CartController.cs
@@ -129,25 +129,6 @@ namespace Spice.Areas.Customer.Controllers
             return RedirectToAction("Index");
         }
 
-        [Authorize]
-        public async Task<IActionResult> OrderHistory()
-        {
-            var claimIdentity = (ClaimsIdentity)User.Identity;
-            var claimUser = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
-            List<OrderDetails> listOrderDetails = new List<OrderDetails>();
-            List<OrderHeader> orderHeaderList = await _db.OrderHeader.Include(s => s.ApplicationUser).Where(s => s.UserId == claimUser.Value).ToListAsync();
-            foreach (OrderHeader item in orderHeaderList)
-            {
-                OrderDetails singleOrderDetail = new OrderDetails
-                {
-                    OrderHeader = item
-                ,   OrderDetail = await _db.OrderDetail.Where(s => s.OrderHeaderId == item.Id).ToListAsync()
-                };
-                listOrderDetails.Add(singleOrderDetail);
-            }
-            return View(listOrderDetails);
-        }
-
         public async Task<IActionResult> Summary()
         {
             // get currentuser
--- Spice/Spice/Areas/Customer/Controllers/OrderController.cs
+++ Spice/Spice/Areas/Customer/Controllers/OrderController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 using Spice.Models.ViewModels;
 
 namespace Spice.Areas.Customer.Controllers
@@ -37,6 +38,26 @@ namespace Spice.Areas.Customer.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [Authorize]
+        public async Task<IActionResult> OrderHistory()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claimUser = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            List<OrderDetails> listOrderDetails = new List<OrderDetails>();
+            List<OrderHeader> orderHeaderList = await _db.OrderHeader.Include(s => s.ApplicationUser).Where(s => s.UserId == claimUser.Value).ToListAsync();
+            foreach (OrderHeader item in orderHeaderList)
+            {
+                OrderDetails singleOrderDetail = new OrderDetails
+                {
+                    OrderHeader = item
+                ,
+                    OrderDetail = await _db.OrderDetail.Where(s => s.OrderHeaderId == item.Id).ToListAsync()
+                };
+                listOrderDetails.Add(singleOrderDetail);
+            }
+            return View(listOrderDetails);
         }
     }
 }
