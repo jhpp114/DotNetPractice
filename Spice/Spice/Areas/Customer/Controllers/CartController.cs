247261a61076164f7015fa1ac431c371e774680f
jhpp114 <jhpp114@uw.edu>
2020-09-22 23:40:40 -0700

    order history index get method

--- Spice/Spice/Areas/Customer/Controllers/CartController.cs
+++ Spice/Spice/Areas/Customer/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -126,6 +127,25 @@ namespace Spice.Areas.Customer.Controllers
             var cnt = _db.ShoppingCart.Where(s => s.ApplicationUserId == cartData.ApplicationUserId).ToList().Count;
             HttpContext.Session.SetInt32("ssCartCount", cnt);
             return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> OrderHistory()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claimUser = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            List<OrderDetails> listOrderDetails = new List<OrderDetails>();
+            List<OrderHeader> orderHeaderList = await _db.OrderHeader.Include(s => s.ApplicationUser).Where(s => s.UserId == claimUser.Value).ToListAsync();
+            foreach (OrderHeader item in orderHeaderList)
+            {
+                OrderDetails singleOrderDetail = new OrderDetails
+                {
+                    OrderHeader = item
+                ,   OrderDetail = await _db.OrderDetail.Where(s => s.OrderHeaderId == item.Id).ToListAsync()
+                };
+                listOrderDetails.Add(singleOrderDetail);
+            }
+            return View(listOrderDetails);
         }
 
         public async Task<IActionResult> Summary()
